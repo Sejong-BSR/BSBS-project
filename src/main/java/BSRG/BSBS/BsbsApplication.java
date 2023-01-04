34f5111df5b679d13265b03f0773cdbe2ff8da86
bob8dod <bob8dod@gmail.com>
2023-01-04 19:31:43 +0900

    first work

---
+++ src/main/java/BSRG/BSBS/BsbsApplication.java
@@ -0,0 +1,13 @@
+package BSRG.BSBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BSRG/BSBS/BsbsApplicationTests.java
@@ -0,0 +1,13 @@
+package BSRG.BSBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
